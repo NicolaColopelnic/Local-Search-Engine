af8f7be5ee5e43c9210b250c500f9d51ae5e0b50
NicolaColopelnic <colopelnic.daria.nicola@gmail.com>
2026-05-23 21:51:21 +0300

    optimize path scoring and synonym decorator with hashmaps

--- src/scanner/PathScore.java
+++ src/scanner/PathScore.java
@@ -1,20 +1,34 @@
 package scanner;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
 
 public class PathScore {
+    private static final Map<String, Double> extensions = new HashMap<>();
+
+    static {
+        extensions.put(".java", 40.0);
+        extensions.put(".md", 20.0);
+        extensions.put(".txt", 10.0);
+        extensions.put(".jpg", 15.0);
+        extensions.put(".png", 15.0);
+        extensions.put(".xml", 5.0);
+    }
+
     public double calculateScore(File file) {
         double score = 0.0;
         String path = file.getAbsolutePath().toLowerCase();
+        String fileName = file.getName().toLowerCase();
 
         if(path.contains("\\src\\") || path.contains("/src/")) {
             score += 50.0;
         }
 
-        if(path.endsWith(".java")) {
-            score += 30.0;
-        } else if(path.endsWith(".md")) {
-            score += 10.0;
+        int lastDot = fileName.lastIndexOf(".");
+        if (lastDot != -1) {
+            String extension = fileName.substring(lastDot);
+            score += extensions.getOrDefault(extension, 0.0);
         }
 
         long depth = path.chars().filter(ch -> ch == '\\' || ch == '/').count();
--- src/search/pipeline/SynonymDecorator.java
+++ src/search/pipeline/SynonymDecorator.java
@@ -1,6 +1,17 @@
 package search.pipeline;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SynonymDecorator extends QueryDecorator {
+
+    private static final Map<String, String> synonymMap = new HashMap<>();
+    static {
+        synonymMap.put("img", "(img OR image OR photo OR jpg OR png OR jfif OR jpeg)");
+        synonymMap.put("doc", "(doc OR documentation OR readme OR md OR txt)");
+        synonymMap.put("code", "(code OR java OR src OR main)");
+    }
+
     public SynonymDecorator(QueryBuilder builder) {
         super(builder);
     }
@@ -8,19 +19,17 @@ public class SynonymDecorator extends QueryDecorator {
     @Override
     public String build(String input) {
         String data = super.build(input);
-        String lower = data.toLowerCase();
 
-        if (lower.toLowerCase().contains("img")) {
-            data = data.replace("img", "(img OR image OR photo OR jpg OR png OR jfif OR jpeg)");
+        for (Map.Entry<String, String> entry : synonymMap.entrySet()) {
+            String keyword = entry.getKey();
+            String expansion = entry.getValue();
+
+            // (?i) makes it case insensitive
+            // \\b makes sure to only match the whole word
+            String regex = "(?i)\\b" + keyword + "\\b";
+            data = data.replaceAll(regex, expansion);
         }
 
-        if (lower.contains("doc")) {
-            data = data.replace("doc", "(doc OR documentation OR readme OR md OR txt)");
-        }
-
-        if (lower.contains("code")) {
-            data = data.replace("code", "(code OR java OR src OR main)");
-        }
         return data;
     }
 }
